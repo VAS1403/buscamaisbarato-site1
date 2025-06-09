4cede3e1e039402edb2daa4938dc05c5ac9de60a
VAS1403 <vanessavas14@gmail.com>
2025-06-09 08:48:31 -0300

    Add files via upload

---
+++ ganhe.js
@@ -0,0 +1,10 @@
+
+export default function Ganhe() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>🎁 Ganhe com Nossos Links</h1>
+      <p>Em breve, links de afiliados Pixbet e muito mais!</p>
+      <a href="/">Voltar para o início</a>
+    </div>
+  );
+}
---
+++ index.js
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>🔎 Busca Mais Barato</h1>
+      <p>Compare preços entre Shopee, Amazon, Magalu, Casas Bahia e mais!</p>
+      <a href="/ganhe">Ir para a página de afiliados</a>
+    </div>
+  );
+}
